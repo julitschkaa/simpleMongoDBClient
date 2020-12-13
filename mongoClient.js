982c39539c01e0f44eea6fc0d49e1e186cc3d907
julitschkaa <schaak@posteo.net>
2020-12-13 13:55:47 +0100

    wrong client oups

--- mongoClient.js
+++ mongoClient.js
@@ -1,9 +1,25 @@
 
+const username = `Admina`;
+const password = `a`;
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://<username>:<password>@cluster0.5ot88.mongodb.net/<AdVizData.Contacts>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const uri = `mongodb+srv://${username}:${password}@cluster0.5ot88.mongodb.net/<AdVizData>?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db('AdVizData');
+    const collection = database.collection('Contacts')
+
+    //Query for a contact that has the name 'cat'
+    const query = {name: 'cat'}
+    const catcontact = await collection.findOne(query);
+
+    console.log(catcontact);
+  } finally{
+    //closing client when finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
